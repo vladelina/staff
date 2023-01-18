06bba3e6a9d1f8880e08f4cac81837f6be6977a1
Vladelina <example@gmail.com>
2023-01-18 19:08:36 +0200

    add project files

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = ({increased, employers}) => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников в компании</h1>
+      <h2>Общее число сотрудников: {employers}</h2>
+      <h2>Премию получат: {increased}</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
